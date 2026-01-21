8a843a4c1de8859a835708f02af0e49e05ba5d8e
ashen <austinszj@gmail.com>
2026-01-21 01:44:14 -0500

    feat: update model

--- config/default.py
+++ config/default.py
@@ -37,8 +37,8 @@ prove_leanserver_num = 64
 # Configuration for the draft model servers. Needs to be replaced before launching.
 draft_model_config = [
     {
-        "base_url": "https://austinszj-3211-resource.openai.azure.com/openai/v1",
-        "api_key": os.environ.get("FOUNDARY_API_KEY"),
+        "base_url": "https://austinszj--vllm-deepseekr1-inference-serve.modal.run",
+        "api_key": "EMPTY",
     }
 ]
 
@@ -54,8 +54,8 @@ draft_sample_config = {
 # Configuration for the sketch model servers. Needs to be replaced before launching.
 sketch_model_config = [
     {
-        "base_url": "https://austinszj-3211-resource.openai.azure.com/openai/v1",
-        "api_key": os.environ.get("FOUNDARY_API_KEY"),
+        "base_url": "https://austinszj--vllm-deepseekv3-inference-serve.modal.run",
+        "api_key": "EMPTY",
     },
 ]
 
